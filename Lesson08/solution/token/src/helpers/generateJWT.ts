ec06609b0fe339c2210f9423caa85b311bcede69
Ali Mohammad <alihmohammad@hotmail.com>
2026-03-26 10:59:31 +0100

    feat: added jti

--- Lesson08/solution/token/src/helpers/generateJWT.ts
+++ Lesson08/solution/token/src/helpers/generateJWT.ts
@@ -1,8 +1,14 @@
 import { SignJWT } from "jose";
+import { randomUUID } from "node:crypto";
 
 export default async function generateJWT(sub: string) {
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const token = await new SignJWT({ sub, role: "user" }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime("15m").sign(secret);
+    const token = await new SignJWT({ sub, role: "user" })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("15m")
+        .setJti(randomUUID())
+        .sign(secret);
 
     return token;
 }
